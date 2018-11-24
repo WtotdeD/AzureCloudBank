9d0ba2521d4874e7ab55b7ab0be50155dbdbac0d
WtotdeD <33087963+WtotdeD@users.noreply.github.com>
2018-11-24 20:02:20 +0100

    Added database

---
+++ AzureFinalDB/Staging.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Datum] date NULL, 
+    [Naam] NVARCHAR(150) NULL, 
+    [Omschrijving] NVARCHAR(MAX) NULL,
+	[Rekening] NVARCHAR(150) NULL,
+	[Tegenrekening] NVARCHAR(150) NULL,
+	[Code] NVARCHAR(150) NULL,
+	[AfBij] NVARCHAR(150) NULL,
+	[Bedrag] MONEY NULL,
+	[Mutatiesoort] VARCHAR(150) NULL,
+	[Mededelingen] VARCHAR(150) NULL,
+)
+
+
+
